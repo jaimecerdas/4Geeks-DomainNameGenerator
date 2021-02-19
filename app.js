03eb7f68ece3bb875b232e6c5e62710e4a922a46
J Cerdas <jaimecerdas@gmail.com>
2021-02-19 02:47:00 +0000

    version with special domains array

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ let pronoun = ['the','our'];
 let adj = ['great', 'big' ];
 let noun = ['jogger','racoon'];
 let extensions = ['.com', '.net', '.us', '.io'];
+let specialDomains = ['on','us'];
 
 let domain = "";
 let domain2 = "";
@@ -13,17 +14,18 @@ pronoun.forEach((element) => {
         domain2 = domain+element;
         noun.forEach((element) => {
             domain3 = domain2+element;
-            // dominios especiales
-            let dominiofinal = domain3.split("");
-            //console.log(dominiofinal)
-            //if (dominiofinal[dominiofinal.length-1]="r"){
-            //    console.log("Yes")
-            let newDomain = (dominiofinal[dominiofinal.length-2]+dominiofinal[dominiofinal.length-1])
-            dominiofinal.pop();
-            dominiofinal.pop();
-            console.log('Fancy Domain Name: '+dominiofinal.join("")+'.'+newDomain)
-            console.log('Regular Domain Names: ')
+            // dominios especiales, Ofrece dominio especial cuando el nombre termina en alguno de los special domains en Array specialDomains
+                let dominiofinal = domain3.split("");
+                let newDomain = (dominiofinal[dominiofinal.length-2]+dominiofinal[dominiofinal.length-1])
+                specialDomains.forEach((element) => {
+                if (newDomain===element){
+                    dominiofinal.pop();
+                    dominiofinal.pop();
+                    console.log('Your domain ends in "'+ element + '" here is a special domain for you: ' +dominiofinal.join("")+'.'+newDomain)
+                }
+                });
 
+            // Agrega todas las extensiones disponibles de un nuevo array.
             extensions.forEach((element) => {
                 domain4 = domain3+element;
                 console.log(domain4)
