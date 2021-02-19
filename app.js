ced9663c86a247c3c160041e23e0f57a308e1d3d
J Cerdas <jaimecerdas@gmail.com>
2021-02-19 03:04:13 +0000

    version al final de clases

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
+// Variables iniciales del ejercicio.
 let pronoun = ['the','our'];
 let adj = ['great', 'big' ];
 let noun = ['jogger','racoon'];
-let extensions = ['.com', '.net', '.us', '.io'];
-let specialDomains = ['on','us'];
-
+// Variable para ejercicios adicionalesgit 
+const extensions = ['.com', '.net', '.us', '.io'];
+const specialDomains = ['on','us'];
+// Variables que guardan cada paso.
 let domain = "";
 let domain2 = "";
 let domain3 = "";
 let domain4= "";
+// Recorre cada elemento para formar el string final.
 pronoun.forEach((element) => {
     domain = element;
     adj.forEach((element) => {
@@ -18,13 +21,12 @@ pronoun.forEach((element) => {
                 let dominiofinal = domain3.split("");
                 let newDomain = (dominiofinal[dominiofinal.length-2]+dominiofinal[dominiofinal.length-1])
                 specialDomains.forEach((element) => {
-                if (newDomain===element){
-                    dominiofinal.pop();
-                    dominiofinal.pop();
-                    console.log('Your domain ends in "'+ element + '" here is a special domain for you: ' +dominiofinal.join("")+'.'+newDomain)
-                }
+                    if (newDomain===element){
+                        dominiofinal.pop();
+                        dominiofinal.pop();
+                        console.log('Your domain ends in "'+ element + '" here is a special domain for you: ' +dominiofinal.join("")+'.'+newDomain)
+                    }
                 });
-
             // Agrega todas las extensiones disponibles de un nuevo array.
             extensions.forEach((element) => {
                 domain4 = domain3+element;
